952a5cd93e92fa122d9cba60fcddefbdfd3c1158
Hong Liu <hong6914@yahoo.com>
2020-10-30 16:55:14 -0700

    Use VS2019 instead, and add one batch script to auto-config the system

---
+++ chapter02/config.cmd
@@ -0,0 +1,61 @@
+@ECHO OFF
+SETLOCAL EnableDelayedExpansion
+
+:: ===================================================================================================
+:: config.cmd
+::
+:: Script to configure debug printer setting in registry, and
+::  turn on signing driver with a test certificate.
+::	Will reboot at the end to let changes to take effect.
+:: ===================================================================================================
+
+SET sErrMessage=
+
+@ECHO.
+@ECHO ----------------------------------------------------------
+@ECHO check to see if you are running the script as a local admin
+net session
+IF %ERRORLEVEL% NEQ 0 (
+    SET sErrMessage=BE SURE you run the script from a DOS prompt as a local admin
+    GOTO :ErrorHandling
+)
+
+@ECHO.
+@ECHO ----------------------------------------------------------
+@ECHO enable DbgPrint in Windows Registry
+REG add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Debug Print Filter" /v DEFAULT /t REG_DWORD /d 8 /f
+IF %ERRORLEVEL% NEQ 0 (
+    SET sErrMessage=Failed to config DbgPrint messaging
+    GOTO :ErrorHandling
+)
+
+@ECHO.
+@ECHO ----------------------------------------------------------
+@ECHO Turn on test signing for drivers
+bcdedit /set testsigning on
+IF %ERRORLEVEL% NEQ 0 (
+    SET sErrMessage=Failed turning on test driver signing
+    GOTO :ErrorHandling
+)
+
+@ECHO.
+@ECHO Press any key to reboot Windows to let the changes take effect,
+@ECHO.
+@ECHO or press Ctrl-C to break from it.
+@ECHO.
+PAUSE
+SHUTDOWN /r /t 0
+
+
+:: ===================================================================================================
+:: Print out the error message and quit
+:: ===================================================================================================
+:ErrorHandling
+@ECHO @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@+++++
+@ECHO.
+@ECHO Error ^>^>^> %sErrMessage%.
+@ECHO.
+@ECHO @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@+++++
+GOTO :EOF
+
+:EOF
